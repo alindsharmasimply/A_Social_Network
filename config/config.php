5d128083083d17b3acc9a254cb9c31c64c630ba1
alindsharmasimply <alindsharmasimply@gmail.com>
2019-04-26 19:08:51 +0530

    Improved some code aesthetics

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+ob_start(); // Starts the output buffering
+// Basically stores the output in an internal buffer
+
+session_start(); // Starts a session which allows to store the session values of variables
+
+// This is how we establish a connection with the database
+$con = mysqli_connect("localhost", "root", "", "social_network");
+// Syntax: mysqli_connect(host, username, password, Database name)
+// $con is a connection variable now
+
+// To display any errors occured while establishing the connection
+if (mysqli_connect_errno()) {
+	echo "Failed to connect " , mysqli_connect_errno();
+}
+ ?>
